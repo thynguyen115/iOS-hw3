3a613a2940a3edb551f7e00c8cb68abdb5418a86
thynguyen115 <77253286+thynguyen115@users.noreply.github.com>
2021-10-05 18:19:21 -0700

    Update HomeTableViewController.swift

--- HomeTableViewController.swift
+++ HomeTableViewController.swift
@@ -8,16 +8,42 @@
 import UIKit
 
 class HomeTableViewController: UITableViewController {
-
+    var tweetArray = [NSDictionary]()
+    var numberOfTweet: Int!
+    let myRefreshControl = UIRefreshControl()
+    
+    
     @IBAction func onLogout(_ sender: Any) {
         TwitterAPICaller.client?.logout()
         self.dismiss(animated: true, completion: nil)
         UserDefaults.standard.set(false, forKey: "userLoggedIn")
     }
+    override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+       if indexPath.row + 1 == tweetArray.count {
+           loadMoreTweets()
+       }
+   }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "tweetCell",  for: indexPath)  as! TweetCellTableViewCell
+        
+        let user = tweetArray[indexPath.row]["user"] as! NSDictionary
+        cell.userNameLabel.text = user["name"] as? String
+        cell.tweetContent.text = tweetArray[indexPath.row]["text"] as? String
+        let imageUrl = URL(string: (user["profile_image_url_https"] as? String)!)
+        
+        let data = try? Data(contentsOf: imageUrl!)
+        if let imageData = data {
+            cell.profileImageView.image  = UIImage(data: imageData)
+        }
+        return cell
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        loadTweets()
+        
+        myRefreshControl.addTarget(self, action: #selector(loadTweets), for: .valueChanged)
+        tableView.refreshControl = myRefreshControl
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
@@ -29,14 +55,52 @@ class HomeTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return tweetArray.count
     }
-
+    
+    
+    @objc func loadTweets(){
+        let myUrl = "https://api.twitter.com/1.1/statuses/home_timeline.json"
+        numberOfTweet = 20
+        let myParams = ["count": numberOfTweet]
+        TwitterAPICaller.client?.getDictionariesRequest(url: myUrl, parameters: myParams, success: {
+            (tweets: [NSDictionary]) in
+            self.tweetArray.removeAll()
+            for t in tweets {
+                self.tweetArray.append(t)
+            }
+            self.tableView.reloadData()
+            self.myRefreshControl.endRefreshing()
+        }, failure: { (Error) in
+        print("Could not retreive tweets! oh no!!")
+    })
+        
+    }
+    
+    func loadMoreTweets() {
+        let myUrl = "https://api.twitter.com/1.1/statuses/home_timeline.json"
+        numberOfTweet = numberOfTweet + 20
+        let myParams = ["count": numberOfTweet]
+        TwitterAPICaller.client?.getDictionariesRequest(url: myUrl, parameters: myParams, success: {
+            (tweets: [NSDictionary]) in
+            self.tweetArray.removeAll()
+            for t in tweets {
+                self.tweetArray.append(t)
+            }
+            self.tableView.reloadData()
+            self.myRefreshControl.endRefreshing()
+        }, failure: { (Error) in
+        print("Could not retreive tweets! oh no!!")
+    })
+    }
+    
+    
+     
     /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
